1bb5742f44679567a0f1d778f9185ae387fe30a9
ivan <b144249b@gmail.com>
2019-08-28 12:11:59 +0800

    Fresh the Ui

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 new Vue({
     el: '#app',
     data: {
-        greeting: 'click select what fking to eat',
+        greeting: '↓↓↓↓↓ Click ↓↓↓↓↓',
         user: 'kkwong',
         city: 'KT'
     },
     methods: {
         changeGreeting() {
-            var num = Math.floor(Math.random() * 6) + 1;
-
+            var items = 10   // <----------- ###Set genName case number here####
+            var num = Math.floor(Math.random() * items) + 1;
             this.greeting = genName(num);
         }
     }
@@ -16,11 +16,15 @@ new Vue({
 
 function genName(num) {
     switch (num) {
-        case 1: return 'Mc Don Don'
-        case 2: return 'tam boy'
+        case 1: return '金  拱  橋'
+        case 2: return '譚 Boy'
         case 3: return '30'
-        case 4: return 'Ban Gor'
-        case 5: return 'red tea'
-        case 6: return '豬米'
+        case 4: return '西  遊  記'
+        case 5: return 'Red Tea'
+        case 6: return '豬  扒  米'
+        case 7: return '1  口  牛'
+        case 8: return 'HK 魚 蛋  檔'
+        case 9: return '地  地  道'
+        case 10: return '西遊記 之 飯'
     }   
 }
